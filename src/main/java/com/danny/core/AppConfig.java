0da313e859d47526a864c1d9af55c30c7dd87914
Danny <dannyyjj123@gmail.com>
2023-02-21 10:28:46 +0900

    AppConfig 리팩터링

--- src/main/java/com/danny/core/AppConfig.java
+++ src/main/java/com/danny/core/AppConfig.java
@@ -1,6 +1,9 @@
 package com.danny.core;
 
+import com.danny.core.discount.DiscountPolicy;
 import com.danny.core.discount.FixDiscountPolicy;
+import com.danny.core.discount.RateDiscountPolicy;
+import com.danny.core.member.MemberRepository;
 import com.danny.core.member.MemberService;
 import com.danny.core.member.MemberServiceImpl;
 import com.danny.core.member.MemoryMemberRepository;
@@ -12,15 +15,20 @@ public class AppConfig {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    private MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+
     public OrderService orderService() {
-        return new OrderServiceImpl(memberRepository(), FixDiscountPolicy());
+        return new OrderServiceImpl(
+                memberRepository(),
+                DiscountPolicy());
     }
 
-    private FixDiscountPolicy FixDiscountPolicy() {
-        return new FixDiscountPolicy();
+    private DiscountPolicy DiscountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
+
 }
