f8f14d95bc398790550069ef3654f2dd95834e37
Danny <dannyyjj123@gmail.com>
2023-02-23 10:59:53 +0900

    Spring으로 전환

--- src/main/java/com/danny/core/AppConfig.java
+++ src/main/java/com/danny/core/AppConfig.java
@@ -9,23 +9,33 @@ import com.danny.core.member.MemberServiceImpl;
 import com.danny.core.member.MemoryMemberRepository;
 import com.danny.core.order.OrderService;
 import com.danny.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+// 객체를 생성하고 관리하면서 의존관계를 연결해 주는 IoC, DI 컨테이너
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean(name = "possibleChangeName")
     public OrderService orderService() {
+
         return new OrderServiceImpl(
                 memberRepository(),
                 DiscountPolicy());
     }
 
-    private DiscountPolicy DiscountPolicy() {
+    @Bean
+    public DiscountPolicy DiscountPolicy() {
+
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
