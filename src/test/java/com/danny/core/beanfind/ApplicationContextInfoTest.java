8645cac6acf45ad8ae59213757c32b2f5bfee173
Danny <dannyyjj123@gmail.com>
2023-02-28 15:41:52 +0900

    Bean 조회

--- src/test/java/com/danny/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/com/danny/core/beanfind/ApplicationContextInfoTest.java
@@ -18,20 +18,22 @@ class ApplicationContextInfoTest {
         // iter + Tab = 리스트 형태가 있으면 자동으로 for문 완성해줌
         for (String beanDefinitionName : beanDefinitionNames) {
             Object been = ac.getBean(beanDefinitionName);
-            System.out.println("name = " + beanDefinitionName + " object = " + been);
+            System.out.println("name = " + beanDefinitionName + " / object = " + been);
         }
     }
     @Test
     @DisplayName("애플리케이션 빈 출력하기")
     void findApplicationBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
-        // iter + Tab = 리스트 형태가 있으면 자동으로 for문 완성해줌
         for (String beanDefinitionName : beanDefinitionNames) {
            BeanDefinition beanDefinition =  ac.getBeanDefinition(beanDefinitionName);
 
+            // Role ROLE_APPLICATION - 직접 등록한 애플리케이션 빈
+            // Role ROLE_INFRASTRUCTURE - 스프링 내부에서 사용하는 빈
             if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
                 Object bean = ac.getBean(beanDefinitionName);
-                System.out.println("bean = " + bean + "object" + bean);
+                System.out.println("Name = " + beanDefinitionName);
+                System.out.println("object = " + bean);
             }
         }
     }
